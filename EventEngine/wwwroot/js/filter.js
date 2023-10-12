7bd7c3ed1194a50eb0332ea352bdde4aa36c1c0a
Emily <emily_clare@live.com>
2023-10-12 15:55:14 +0200

    filter dropdown functionality

---
+++ EventEngine/wwwroot/js/filter.js
@@ -0,0 +1,16 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var filterToggle = document.getElementById("filter-toggle");
+    var filterSection = document.querySelector(".filter-section");
+
+    filterToggle.addEventListener("click", function () {
+        filterSection.classList.toggle("active");
+    });
+});
+
+function clearFilters() {
+    document.querySelector('input[name="Search"]').value = ''; // Clear the search input
+    document.querySelector('input[name="IsIndoor"]').checked = false; // Uncheck the Indoor Events checkbox
+    document.querySelector('select[name="categoryFilter"]').value = ''; // Clear the Category filter
+    document.querySelector('select[name="sortCriteria"]').value = 'Title'; // Reset sorting to default
+    document.querySelector('form').submit(); // Submit the form to reload the page
+}
